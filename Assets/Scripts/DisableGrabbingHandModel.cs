85ee8de033280883c7010210ba612af31f370490
RosoVRgarden <68047863+RosoVRgarden@users.noreply.github.com>
2023-11-03 13:10:22 +0800

    Hide hands while selecting

---
+++ Assets/Scripts/DisableGrabbingHandModel.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class DisableGrabbingHandModel : MonoBehaviour
+{
+    public GameObject leftHandModel;
+    public GameObject rightHandModel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        XRGrabInteractable grabInteractable = GetComponent<XRGrabInteractable>();
+        grabInteractable.selectEntered.AddListener(HideGrabbingHand);
+        grabInteractable.selectExited.AddListener(ShowGrabbingHand);
+    }
+
+    public void HideGrabbingHand(SelectEnterEventArgs args)
+    {
+        if(args.interactorObject.transform.tag == "Left Hand")
+        {
+            leftHandModel.SetActive(false);
+        }
+        else if(args.interactorObject.transform.tag == "Right Hand")
+        {
+            rightHandModel.SetActive(false);
+        }
+    }
+
+    public void ShowGrabbingHand(SelectExitEventArgs args)
+    {
+        if (args.interactorObject.transform.tag == "Left Hand")
+        {
+            leftHandModel.SetActive(true);
+        }
+        else if (args.interactorObject.transform.tag == "Right Hand")
+        {
+            rightHandModel.SetActive(true);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
