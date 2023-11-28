d38821bd567bcc2c196fdabf8ed816352ecbeb6c
RosoVRgarden <68047863+RosoVRgarden@users.noreply.github.com>
2023-11-28 14:26:19 +0800

    ColorScript

---
+++ Assets/Scripts/ColorObject.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ColorObject : MonoBehaviour
+{
+
+    public InputActionReference colorReference = null;
+    private MeshRenderer meshRenderer = null;
+
+    void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float value = colorReference.action.ReadValue<float>();
+        UpdateColor(value);
+    }
+
+    void UpdateColor(float value)
+    {
+        meshRenderer.material.color = new Color(value, value, value);
+    }
+}
